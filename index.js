8b737ef386a97a2f397b57d5082d5858a57f048d
M'mah Zombo <www.mariamzombo@gmail.com>
2024-02-03 20:54:42 +0000

    made the create to page to create and store blog posts in the database

--- index.js
+++ index.js
@@ -2,44 +2,56 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const BlogPost = require('./models/BlogPost.js');
 
 const app = express(); // Remove 'new' from express()
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); // Use path.join for path concatenation
 
-mongoose.connect('mongodb://localhost/your_database', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true });
 
 app.use(express.static('public'));
 
- app.get('/', (req, res) => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', async (req, res) => {
     // res.sendFile(path.resolve(__dirname, 'views/index.html'));
-    res.render('index');
- })
+    const blogPosts = await BlogPost.find({});
+    res.render('index', { blogPosts });
+    // console.log(blogPosts);
+});
 
 app.get('/about', (req, res) => {
     // res.sendFile(path.resolve(__dirname, 'views/about.html'));
     res.render('about');
-})
+});
 
 app.get('/contact', (req, res) => {
     // res.sendFile(path.resolve(__dirname, 'views/contact.html'));
     res.render('contact');
-})
+});
 
 app.get('/post', (req, res) => {
     // res.sendFile(path.resolve(__dirname, 'views/post.html'));
     res.render('post');
-})
+});
 
 app.get('/posts/new', (req, res) => {
     // res.sendFile(path.resolve(__dirname, 'views/create.html'))
     res.render('create');
-})
+});
 
 app.post('/posts/store', (req, res) => {
-    // 
-})
+    // console.log(req.body);
+    BlogPost.create(req.body)
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err));
+ 
+});
+
 app.listen(4000, () => {
     console.log("Server started on port 4000");
-})
+});
