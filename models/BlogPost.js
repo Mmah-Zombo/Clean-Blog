cc6b82c62aca2457048bcc0ebf68ed06f956ba42
M'mah Zombo <www.mariamzombo@gmail.com>
2024-02-08 16:59:12 +0000

    updated the BlogPost table and model to store  image input

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -9,7 +9,8 @@ const BlogPostSchema = new Schema({
     datePosted: {
         type: Date,
         default: new Date
-    }
+    },
+    image: String
 });
 
 // Creates a model our we will use to interact with the table
