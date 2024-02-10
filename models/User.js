920d533f31d55fd8f47a784cbc76d0f7fe81ddcb
M'mah Zombo <www.mariamzombo@gmail.com>
2024-02-10 04:15:41 +0000

    encrypted user password

--- models/User.js
+++ models/User.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
     username: String,
     password: String
+})
+
+// Using arrow function with 'next' won't work in this case
+UserSchema.pre('save', function(next) {
+    const user = this;
+
+    bcrypt.hash(user.password, 10, (error, hash) => {
+        user.password = hash;
+        next();
+    })
 })
 
 const User = mongoose.model('User', UserSchema);
