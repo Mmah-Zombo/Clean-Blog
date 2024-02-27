e4bb985011adc0502e9b3b04fb25a543213f87c1
M'mah Zombo <www.mariamzombo@gmail.com>
2024-02-27 20:20:49 +0000

    added a text editor to the blog

--- controllers/newPost.js
+++ controllers/newPost.js
@@ -1,15 +1,7 @@
 module.exports = (req, res) => {
     if (req.session.userId) {
-        // const data = req.flash('data')[0];
-        // let title = "";
-        // let body = "";
-        // let image = "";
-        // if (data) {
-        //     title = data.title;
-        //     body = data.body;
-        //     image = data.image;
-        // }
-        return res.render('create',  { errors: req.flash('validationErrors')});
+
+        return res.render('create',  { errors: req.flash('validationErrors'), createPost: true});
     }
 
     res.redirect('/auth/login');
