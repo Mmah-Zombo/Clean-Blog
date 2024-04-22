2383467b97e5f0334e570720c0935282a452bab4
Mmah-Zombo <www.mariamzombo@gmail.com>
2024-04-22 21:24:57 +0000

    began polishing the app for deployment

--- controllers/getPost.js
+++ controllers/getPost.js
@@ -1,6 +1,6 @@
 const BlogPost = require('../models/BlogPost');
 
 module.exports = async (req, res) => {
-    const blogpost = await BlogPost.findById(req.params.id);
+    const blogpost = await BlogPost.findById(req.params.id).populate('userid');
     res.render('post', { blogpost });
 }
--- controllers/storeUserController.js
+++ controllers/storeUserController.js
@@ -2,7 +2,12 @@ const User = require('../models/User');
 
 module.exports = (req, res) => {
     User.create(req.body)
-    .then(() => res.redirect('/'))
+    .then((user) => {
+        const successMessage = "You have successfully created an account";
+        req.flash('successMessage', successMessage);
+        req.session.userId = user._id;
+        res.redirect('/');
+    })
     .catch(err => {
         const validationErrors = Object.keys(err.errors).map(key => err.errors[key].message);
         req.flash('validationErrors', validationErrors);
